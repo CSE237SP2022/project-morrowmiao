8c8859b93221afcea09f2b8dfb56ad978557fe52
kmorrow1 <71241090+kmorrow1@users.noreply.github.com>
2022-03-30 02:09:35 -0500

    Work in progress

---
+++ parkingLotProgram.java
@@ -0,0 +1,13 @@
+package parkingLotSpotPick;
+import support.cse131.ArgsProcessor;
+public class parkingLotProgram {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArgsProcessor ap = new ArgsProcessor(args);
+
+	}
+
+}
